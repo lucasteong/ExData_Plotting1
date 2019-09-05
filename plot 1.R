3a599e33ec28629862fba92bda22ddb9ae752ddc
Lucas Teong <34517145+lucasteong@users.noreply.github.com>
2019-09-05 15:45:02 +0800

    Create plot 1.R

---
+++ plot 1.R
@@ -0,0 +1,32 @@
+#########################################
+# 1 - Download data to working directory
+#########################################
+
+# Download zipfile if it does not exists
+filename <- "exdata_data_household_power_consumption.zip"
+
+if (!file.exists(filename)){
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", filename, method="curl")
+}else{
+  print("Zip file already exists.")
+}
+
+# Checking if file is unzipped in working directory
+if (!file.exists("household_power_consumption.txt")) { 
+  unzip(filename) 
+} else{
+  print("File already exists.")
+}
+
+#########################################
+# 2 - Read data
+#########################################
+
+household <- read.table("household_power_consumption.txt", sep =";", header = TRUE)
+household <- subset(household,household$Date=="1/2/2007" | household$Date =="2/2/2007")
+
+#########################################
+# 2 - Plotting histogram
+#########################################
+hist(as.numeric(as.character(household$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+
